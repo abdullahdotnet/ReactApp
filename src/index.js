b3a89d5d6216fd86a057edbf217a4e25abccde97
Muhammad Abdullah Ghazi <abdullahdotnet20@gmail.com>
2024-03-08 06:40:42 -0800

    changes in current project

--- src/index.js
+++ src/index.js
@@ -1,33 +1,73 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+
+const Books = [
+  {
+    title: 'House of flame and shadows',
+    author: 'Sarah j. Maas',
+    image: './images/newbook.jpg',
+    id: 1,
+  },
+  {
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    image: './images/atomichabits.jpg',
+    id: 2,
+  },
+  {
+    title: 'Fourth Wing',
+    author: 'Rebecca Yaros',
+    image: './images/fourthwing.jpg',
+    id: 3,
+  },
+]
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      {Books.map((book) => {
+        const { title, author, image, id } = book
+        return <Book image={image} author={author} title={title} key={id} />
+      })}
     </section>
   )
 }
 
-const Book = () => {
+// const Book = () => {
+//   return (
+//     <article className="book">
+//       <Image />
+//       <Title />
+//       <Author />
+//     </article>
+//   )
+// }
+const Book = (props) => {
+  const { title, author, image } = props
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <h4
+        style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}
+      >
+        {author}
+      </h4>
     </article>
   )
 }
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/91n7p-j5aqL._SY342_.jpg"
-    alt="House of flame and shadows"
-  />
-)
-const Title = () => <h2>House of flame and shadows</h2>
-const Author = () => <h4>Sarah j. Maas</h4>
+// const Image = () => (
+//   <img
+//     src="https://m.media-amazon.com/images/I/91n7p-j5aqL._SY342_.jpg"
+//     alt="House of flame and shadows"
+//   />
+// )
+// const Title = () => <h2>House of flame and shadows</h2>
+// const Author = () => (
+//   <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>
+//     Sarah j. Maas
+//   </h4>
+// )
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
